ccfef566b97d0b2171603308e8d04056a1ff7b4c
Gabryelle SS <gabryelle-gaby@hotmail.com>
2022-06-15 21:15:16 -0300

    feat: styles sidebar
    Resolves: main

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,13 @@
-import { sidebar, cover, profile } from './styles.module.css';
+import { 
+  sidebar, 
+  cover, 
+  profile, 
+  avatar, 
+  userName,
+  userProfession,
+  footer,
+  editProfileBtn,
+} from './styles.module.css';
 import { AiOutlineEdit } from "react-icons/ai";
 
 export const Sidebar = () => {
@@ -9,12 +18,18 @@ export const Sidebar = () => {
         src="https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
       />
       <div className={profile}>
-        <strong>Gabryelle Silva</strong>
-        <span>Web Developer</span>
+        <img 
+          className={avatar}
+          src="https://github.com/GabryelleSS.png" 
+        />
+        <strong className={userName}>Gabryelle Silva</strong>
+        <span className={userProfession}>Web Developer</span>
       </div>
 
-      <footer>
-        <a href="#">
+      <footer
+        className={footer}
+      >
+        <a href="#" className={editProfileBtn}>
           <AiOutlineEdit />
           Editar seu perfil
         </a>
